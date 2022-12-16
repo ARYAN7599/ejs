506505393df38fe184b4004a28bc9468273c0f24
Aryan singh <115468672+ARYAN7599@users.noreply.github.com>
2022-12-16 17:06:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-ssh root@159.65.147.222<<EOF
-   cd /var/www/html/
-   git pull origin main
-   npm install --production
-   pm2 restart all
-   exit
-EOF
+cd /var/www/html/
+git pull origin main
+npm install --production
+pm2 restart all
